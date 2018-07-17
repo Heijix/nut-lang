63d6ace45f8f157d8cd52f994e0db7734c267f48
Xavitator <xavier75013@gmail.com>
2018-07-17 18:12:12 +0200

    add of gradle

---
+++ core/java/src/Nut.java
@@ -0,0 +1,8 @@
+public class Nut{
+
+    public static int main(String[] args){
+
+        return (args.length);
+    }
+
+}
---
+++ test_utils/core/java/test/TestNut.java
@@ -0,0 +1,10 @@
+import org.junit.*;
+
+public class TestNut{
+
+
+    public void main() {
+        Assert.assertEquals("must return 0",Nut.main(new String[0]),0);
+    }
+
+}
